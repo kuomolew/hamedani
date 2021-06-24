a4c4452ede309bc3d5cf1a72bad5c4c4498c54e5
Vladimir Mironov <kuomolew@gmail.com>
2021-06-24 23:02:41 +0300

    classes

---
+++ c-basics/chapter1.4-non-primitives/Classes/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using Classes.Math;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            var person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Smith";
+            person.Introduce();
+
+            var result = Calculator.Add(1, 2);
+            System.Console.WriteLine(result);
+        }
+    }
+}
