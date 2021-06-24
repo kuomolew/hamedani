763875609d87f751310ef678c1673bac7169be58
Vladimir Mironov <kuomolew@gmail.com>
2021-06-24 23:23:14 +0300

    arrays

---
+++ c-basics/chapter1.4-non-primitives/Array/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new int[3];
+
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var strings = new string[3] { "John", "Snow", "" };
+
+            Console.WriteLine(strings[0]);
+            Console.WriteLine(strings[1]);
+            Console.WriteLine(strings[2]);
+
+            var flags = new bool[3];
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+        }
+    }
+}
