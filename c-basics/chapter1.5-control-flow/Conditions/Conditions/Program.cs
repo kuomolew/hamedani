9406670fe8601f3d1a0461c2c05d6831f02b7c99
Vladimir Mironov <kuomolew@gmail.com>
2021-06-25 23:12:09 +0300

    conditions

---
+++ c-basics/chapter1.5-control-flow/Conditions/Conditions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Conditions
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning!");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon!");
+            }
+            else
+            {
+                Console.WriteLine("Its evening!");
+            }
+
+            bool isGoldCustomer = true;
+            float price;
+
+            if (isGoldCustomer)
+                price = 19.95f;
+            else
+                price = 29.95f;
+
+            price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);
+
+            var season = Season.Summer;
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn");
+                    break;
+                case Season.Spring:
+                case Season.Summer:
+                    Console.WriteLine("We've got a promotion!");
+                    break;
+                default:
+                    Console.WriteLine("It's winter");
+                    break;
+            }
+        }
+    }
+}
---
+++ c-basics/chapter1.5-control-flow/Conditions/Conditions/Season.cs
@@ -0,0 +1,13 @@
+﻿namespace Conditions
+{
+    partial class Program
+    {
+        public enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter,
+        }
+    }
+}
