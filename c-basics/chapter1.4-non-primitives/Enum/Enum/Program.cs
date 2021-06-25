9d3415d390028a993e68e65b7c62e9816b972214
Vladimir Mironov <kuomolew@gmail.com>
2021-06-25 12:36:04 +0300

    enum

---
+++ c-basics/chapter1.4-non-primitives/Enum/Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enum
+{
+    public enum ShippingMethod
+    {
+        RegularMail = 1,
+        RegisteredMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)System.Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
