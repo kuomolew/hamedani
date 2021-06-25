c34778b35eb032f8a411642c4ab4cb736fb2345e
Vladimir Mironov <kuomolew@gmail.com>
2021-06-26 00:11:49 +0300

    random

---
+++ c-basics/chapter1.5-control-flow/RandomClass/RandomClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            var random = new Random();
+            for(var i = 0; i < 10; i++)
+                Console.WriteLine(random.Next(1, 10));
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
