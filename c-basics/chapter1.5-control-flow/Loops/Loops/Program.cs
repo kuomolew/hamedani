7a8a0da33dac2681c712c208e4c8c6e79674401e
Vladimir Mironov <kuomolew@gmail.com>
2021-06-25 23:54:08 +0300

    loops

---
+++ c-basics/chapter1.5-control-flow/Loops/Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            //for (var i = 10; i >= 1; i-- )
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine(input);
+                    continue;
+                }
+                break;
+                
+            }
+
+            var name = "John Smith";
+            foreach(var character in name)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
