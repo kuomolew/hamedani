1e4e799ab6b91d12bf076106bc17626f015040a7
Vladimir Mironov <kuomolew@gmail.com>
2021-07-02 15:24:22 +0300

    dateTime

---
+++ c-basics/chapter1.7-DateTime/DateTime/DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dateTime = new System.DateTime(2015, 1, 1);
+            var now = System.DateTime.Now;
+            var today = System.DateTime.Today;
+
+            Console.WriteLine($"Hours: {now.Hour}");
+            Console.WriteLine($"Minutes: {now.Minute}");
+
+            var tomorrow = now.AddDays(1);
+            var yesterdat = now.AddDays(-1);
+
+            Console.WriteLine($"Tomorrow: {now.ToString()}");
+            Console.WriteLine($"To string formatter: {now.ToString("yy-MM-dd HH:mm")}");
+            Console.WriteLine($"Tomorrow: {now.ToLongDateString()}");
+            Console.WriteLine($"Tomorrow: {now.ToShortDateString()}");
+            Console.WriteLine($"Tomorrow: {now.ToLongTimeString()}");
+            Console.WriteLine($"Tomorrow: {now.ToShortTimeString()}");
+            
+        }
+    }
+}
