71a9a7300797466a7935a882bfb6f14c4b5ee4b6
Vladimir Mironov <kuomolew@gmail.com>
2021-07-02 17:50:14 +0300

    summarising text

---
+++ c-basics/chapter1.8-Strings/SummarisingText/SummarisingText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarisingText
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sentence = "This is going to be a very very very very very vere really very very long text.";
+            string summary = StringUtility.SummarizeText(sentence, 20);
+
+            Console.WriteLine(summary);
+            Console.WriteLine(summary.Length);
+        }
+    }
+}
---
+++ c-basics/chapter1.8-Strings/SummarisingText/SummarisingText/StringUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SummarisingText
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+            string summarySentence = text;
+
+            if (text.Length > maxLength)
+            {
+                var words = text.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                    {
+                        break;
+                    }
+                    summaryWords.Add(word);
+                }
+
+                summarySentence = String.Join(" ", summaryWords) + "...";
+            }
+            return summarySentence;
+        }
+    }
+}
