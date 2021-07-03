a6f6430c2714a99269f23a9d830688c6e83ba4fe
Vladimir Mironov <kuomolew@gmail.com>
2021-07-03 19:14:16 +0300

    directory

---
+++ c-basics/chapter1.9-Files/Directory/Directory/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Directory
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.IO.Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = System.IO.Directory.GetFiles(@"c:\temp", "*.*", SearchOption.AllDirectories);
+            foreach(var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            
+            if (System.IO.Directory.Exists(@"c:\temp"))
+            {
+                var directories = System.IO.Directory.GetDirectories(@"c:\temp", "*.*", SearchOption.AllDirectories);
+                foreach (var directory in directories)
+                    Console.WriteLine(directory); 
+            }
+        }
+    }
+}
