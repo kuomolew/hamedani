abea9f17c1e0b04b92884837b6da56d483c106b5
Vladimir Mironov <kuomolew@gmail.com>
2021-07-03 19:14:22 +0300

    path

---
+++ c-basics/chapter1.9-Files/Path/Path/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Path
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = @"c:\temp\myfile.jpg";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            extension = System.IO.Path.GetExtension(path);
+            var fileName = System.IO.Path.GetFileName(path);
+            var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(path);
+            var directoryName = System.IO.Path.GetDirectoryName(path);
+
+            Console.WriteLine(extension);
+            Console.WriteLine(fileName);
+            Console.WriteLine(fileNameWithoutExtension);
+            Console.WriteLine(directoryName);
+
+
+        }
+    }
+}
