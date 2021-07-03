fe299a666c8c5c61fbb28634458f4c3acfcbdae4
Vladimir Mironov <kuomolew@gmail.com>
2021-07-03 19:14:07 +0300

    files

---
+++ c-basics/chapter1.9-Files/FileFileInfo/FileFileInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = @"c:\temp\myfile1.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+
+            if (File.Exists(path))
+                File.Delete(path);
+
+            File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo(@"d:\temp\myfile.jpg", true);
+            if (fileInfo.Exists)
+                fileInfo.Delete();
+
+            
+        }
+
+    }
+}
