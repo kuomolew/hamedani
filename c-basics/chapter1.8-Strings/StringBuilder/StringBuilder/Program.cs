ae0fd54301d5195e7f6cc1591f8e41f1c45f2b8e
Vladimir Mironov <kuomolew@gmail.com>
2021-07-03 16:22:42 +0300

    string builder

---
+++ c-basics/chapter1.8-Strings/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main()
+        {
+            var str = "Hello World";
+            var builder = new System.Text.StringBuilder(str);
+            builder.Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+            Console.WriteLine(builder[0]);
+        }
+    }
+}
